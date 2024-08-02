b23e36f9f84ba7fbb18d35727a492e9e484a8aad
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-08-02 09:30:13 +0530

    DEPLOY BUG: ADDED CONFIGURATION FILE

--- app/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ app/src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -17,5 +17,4 @@ public class SpringBootDataJpaApplication {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
 
-
 }
---
+++ app/src/main/java/com/bezkoder/spring/datajpa/configuration/DatabaseConfiguration.java
@@ -0,0 +1,38 @@
+package com.bezkoder.spring.datajpa.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Value("${MYSQLDB_ROOT_URL}")
+    private String databaseUrl;
+
+    @Value("${MYSQLDB_USER}")
+    private String username;
+
+    @Value("${MYSQLDB_ROOT_PASSWORD}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        System.out.println("Database URL"+databaseUrl);
+        System.out.println("Username :"+username);
+        System.out.println("Password :"+password);
+        HikariConfig hikariConfig = new HikariConfig();
+
+            hikariConfig.setJdbcUrl("jdbc:"+databaseUrl);
+            hikariConfig.setUsername(username);
+            hikariConfig.setPassword(password);
+            hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+            return new HikariDataSource(hikariConfig);
+
+    }
+}
